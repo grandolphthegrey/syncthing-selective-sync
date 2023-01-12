f94ab1d9e4dc580f11bfa4479f8ba76940674c5e
grandolphthegrey <46690353+grandolphthegrey@users.noreply.github.com>
2023-01-11 16:51:31 -0800

    Add files via upload

---
+++ syncthing_conflicts.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  6 22:15:46 2022
+
+@author: George
+"""
+
+import requests
+import subprocess
+import socket
+
+
+#API Keys
+
+#Hostname and API Keys for each node
+if socket.gethostname() == '':
+    api_key = ''
+    
+elif socket.gethostname() == '':
+    api_key = ''
+
+#Get all folder IDs currently being synced
+r = requests.get('http://127.0.0.1:8384/rest/config/folders', headers={'X-API-Key':api_key})
+
+#Loop through folder IDs and get human-readable pathnames
+for folder in r.json():
+    
+    path = folder['path']
+    
+    #Search for any files with "sync-conflict" in them
+    cmd = "find '{}' -name '*.sync-conflict-*'".format(path)
+    
+    p = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
+    
+    #If the output is greater than 0, then there is at least on sync conflict file
+    if len(p.stdout.decode()) > 0:
+        
+        msg = p.stdout.decode()
+        
+        #Replace the absolute user path with a tilde to condense space in the notification
+        try:
+            msg = msg.replace('/Users/', '~')
+        except:
+            continue
+        
+        #Show a notification for each sync-conflict file.
+        cmd = '/usr/local/bin/terminal-notifier -title "SyncThing Conflict" -message "{}" -sender com.github.xor-gate.syncthing-macosx'.format(msg)
+        
+        p = subprocess.run(cmd, shell=True)
